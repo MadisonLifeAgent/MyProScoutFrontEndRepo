c8c59c9be1d8e93f5e85fdb7d25f798e632d37f3
Fue Moua <MadisonLifeAgent@gmail.com>
2021-09-14 20:17:28 -0500

    added delete team function

--- src/components/MyTeams/DisplayTeams.js
+++ src/components/MyTeams/DisplayTeams.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 // Component or hook imports
 import UseGetTeams from "../../hooks/UseGetTeams";
@@ -10,11 +11,30 @@ const DisplayTeams = (props) => {
     console.log("teams");
     console.log(teams);
 
+    async function deleteTeam(teamId) {
+        try{
+            let response = await axios.delete(`https://localhost:44394/api/baseballteams/delete/${teamId}`);
+            // if good api call ste the teamRegions
+            console.log("team deleted");
+            window.location = '/myteams';
+        }
+        catch(ex) {
+            console.log("bad api call");
+        }
+    }
+    
+
     // display all teams
     const showTeams = teams.map((item) => {
         return (
             <React.Fragment>
-                <li>{item.baseballTeamName}</li>
+                <dt>{item.baseballTeamName}</dt>
+                    <dd>Level: {item.competitionLevelName}</dd>
+                    <dd>Region: {item.regionName}</dd>
+                    <input type="button" value="Delete Team" onClick={(event) => deleteTeam(item.baseballTeamId)} />
+                    <br/><br/>
+
+
             </React.Fragment>
         );
     })
@@ -25,9 +45,9 @@ const DisplayTeams = (props) => {
         return (
             <div>
                 <h3>myTeams List</h3>
-                <ul>
+                <dl>
                     {showTeams}
-                </ul>
+                </dl>
             </div>
         )
     }
