86477bf154fb44e46531348c2bfba8f472230499
Fue Moua <MadisonLifeAgent@gmail.com>
2021-09-18 22:32:28 -0500

    add DisplayMyPlayers.

---
+++ src/components/MyPlayers/DisplayMyPlayers.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+
+// Component or hook imports
+//import UseGetPlayers from "../../hooks/UseGetPlayers";
+
+// this component gets players and displays them
+const DisplayMyPlayers = (props) => {
+    const scout = props.scout;
+
+    const [players, setPlayers] = useState([]);
+
+
+    async function getMyPlayers(id) {
+        try{
+            let response = await axios.get(`https://localhost:44394/api/playerscoutjoin/${id}`);
+
+            console.log("good call");
+            setPlayers(response.data);
+        }
+        catch(ex) {
+            console.log("bad api call");
+        }
+    }
+
+    useEffect(() => {
+        getMyPlayers(scout.id);
+    },[]);
+
+    console.log(players);
+
+
+
+    // display all players and edit/delete buttons
+    const showPLayers = players.map((item) => {
+        const playerProfileId = item.playerProfile.playerProfileId;
+        return (
+            <React.Fragment>
+                <dt>{item.playerProfile.firstName} {item.playerProfile.lastName}</dt>
+                    <dd>Position: {item.playerProfile.playerPositionPrimaryName}</dd>
+                    <dd>Batthing Hand: {item.playerProfile.playerBattingHandednessName}</dd>
+                    <dd>Throwing Hand: {item.playerProfile.playerThrowingHandednessName}</dd>
+
+                    <Link class="btn btn-primary me-3" to={{
+                        pathname: `/myplayers/playerprofile`,
+                        state: {
+                            player: item.playerProfile,
+                        }
+                    }}>View Profile</Link>
+
+                    <Link class="btn btn-primary me-3" to={{
+                        pathname: `/myplayers/editplayer`,
+                        state: {
+                            player: item.playerProfile,
+                        }
+                    }}>Edit Profile</Link>
+
+                    
+{/*                     <input type="button" class="btn btn-primary" value="Delete Player" onClick={(event) => deletePlayer(item.playerProfileId)} />
+ */}
+                    <br/><br/>
+            </React.Fragment>
+        );
+    })
+
+
+
+    // displays team, regions, competition levels and create or edit buttons
+        return (
+            <div>
+                <dl>
+                    {showPLayers}
+                </dl>
+            </div>
+        )
+    }
+    // return profile page is user logged in
+
+
+
+
+export default DisplayMyPlayers;
--- src/components/MyPlayers/MyPlayers.js
+++ src/components/MyPlayers/MyPlayers.js
@@ -1,14 +1,29 @@
 import React from "react";
+import jwtDecode from "jwt-decode";
 
 // Component or hook imports
+import DisplayMyPlayers from "./DisplayMyPlayers";
 
 // displays players belong to a scout
 const MyPlayers = (props) => {
+    const jwt = localStorage.getItem('token');
+    function getUser() {
+        try{
+            const user = jwtDecode(jwt);
+            console.log(user);
+            return user;
+        } catch {
+        }
+    }
+
+    const scout = getUser();
    
 // displays team, regions, competition levels and create or edit buttons
     return (
         <div>
             <h3>myPlayers</h3>
+            <DisplayMyPlayers scout={scout} />
+            
         </div>
     )
 }
