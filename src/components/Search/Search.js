ab2508293fa4ebbfa29ea56668af7f5aa9718253
Fue Moua <MadisonLifeAgent@gmail.com>
2021-09-20 12:12:27 -0500

    added styling to search bar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 // Component or hook imports
+import './Search.css';
 
 // Page to search for players
 const Search = (props) => {
@@ -32,29 +33,41 @@ const Search = (props) => {
     
      // display search bar and drop down option
         return (
-            <div>
-                <h3>Player Search</h3>
-                <form >
-                    <select class="form-select" name="category" aria-label="Default select example" onChange={onSelect}>
-                        <option value={player} selected >Player</option>
-                        <option value={position} >Position Search</option>
-                        <option value={team} >Team</option>
-                        <option value={region} >Region</option>
-                        <option value={competitionLevel} >Competition Level</option>
-                        <option value={battingHand} >Batting Handedness</option>
-                        <option value={throwingHand} >Throwing Handedness</option>
-                        <option value={playerType} >Player Type</option>
-                    </select>
-                    <input type="text" name="searchTerm"  onChange={handleChange}/>
+            <div id="forms">
+                <h3 id="pagetitle">Player Search</h3>
+                <div class="row">
+                    
+                    <form class="row">
+                        <div class="input-group mb-3 col-auto">    
+                            <div class="col-sm-auto col-lg-auto">
+                                <select class="form-select" id="selectbutton" name="category" aria-label="Default select example" onChange={onSelect}>
+                                    <option value={player} selected >Player</option>
+                                    <option value={position} >Position Search</option>
+                                    <option value={team} >Team</option>
+                                    <option value={region} >Region</option>
+                                    <option value={competitionLevel} >Competition Level</option>
+                                    <option value={battingHand} >Batting Handedness</option>
+                                    <option value={throwingHand} >Throwing Handedness</option>
+                                    <option value={playerType} >Player Type</option>
+                                </select>
+                            </div>
+                            <div class="col-sm-auto col-lg-6 ms-1">
+                                <input type="text" id="inputfields" name="searchTerm" class="form-control" onChange={handleChange}/>
+                            </div>
 
-                    <Link class="btn btn-primary ms-3" type="submit" to={{
-                        pathname: `/search/results:${searchTerm}:${category}`,
-                        state: {
-                            searchTerm: searchTerm,
-                            category: category,
-                        }
-                    }}>Search</Link>
-                </form>
+                            <div class="col-sm-auto col-lg-auto ms-1">
+                                <Link id="newnotebutton" class="form-control text-center" type="submit" to={{
+                                    pathname: `/search/results:${searchTerm}:${category}`,
+                                    state: {
+                                        searchTerm: searchTerm,
+                                        category: category,
+                                    }
+                                }}>Search</Link>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
 
             </div>
         )
